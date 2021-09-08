9fe19889d5f7ca824d5cb3d48761138ba2741e2c
cesaraph <capimiento@utp.edu.co>
2021-09-08 15:55:25 -0500

    checking sh script_threads

--- script_threads.sh
+++ script_threads.sh
@@ -1,12 +1,13 @@
 #! /mnt/c/Users/cesar/Documents/HPC
 for i in 1 2 3 4 5 6 7 8 9 10
 do  
+    echo "Try $i \n">>result_threads.txt
     for k in 2 4 6 8
     do
-        echo "Try $i \n">>result_threads.txt
+        echo "with $k threads">>result_threads.txt
         for j in 16 200 400 600
         do
-            echo "Matrix's size $j x $j with $k threads: ">>result_threads.txt
+            echo "Matrix's size $j x $j : ">>result_threads.txt
             gcc -pthread matrix_thread.c -o output
             ./output $j $k
         done
