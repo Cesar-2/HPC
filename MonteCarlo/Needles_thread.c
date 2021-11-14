e51ad4d0a4c52575e842e015de7047d7f4e09f88
Cesar-2 <capimiento@utp.edu.co>
2021-11-14 13:06:41 -0500

    ptherad implemented

--- MonteCarlo/Needles_thread.c
+++ MonteCarlo/Needles_thread.c
@@ -2,38 +2,34 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
-#include <error.h>
 #include <pthread.h>
 
 #ifndef MPI
 #define M_PI 3.14159265358979323846
 #endif
-#define MAX_THREADS 8
+long long iterations;
+int num_of_threads;
+long long n_crossed = 0;
+double l;
+void *monte_carlo(void *rank);
 
-pthread_mutex_t mutex;
-long long add = 0;
-typedef struct
+int main(int argc, char **argv)
 {
-    long long iterations;
-    long long total_iterations;
-    double l;
-    int thread;
-} structure_data;
+    l = atoi(argv[1]);
+    iterations = atoi(argv[2]);
+    int thread = atoi(argv[3]);
 
-void monte_carlo(structure_data *, pthread_t *, double, long long);
-void *needles(void *structure_data);
-
-int main(int argc, char *argv[])
-{
-    srand(time(NULL));
-    double l = atof(argv[1]);
-    long long iterations = atoll(argv[2]);
     double p, pi;
-    structure_data *data = NULL;
-    pthread_t *threads = (pthread_t *)malloc(MAX_THREADS * sizeof(pthread_t));
+    long rank;
+    num_of_threads = thread;
+    pthread_t tid[thread];
+    srand(time(NULL));
     clock_t start = clock();
-    monte_carlo(data, threads, l, iterations);
-    p = add / (double)iterations;
+    for (rank = 0; rank < num_of_threads; rank++)
+        pthread_create(&tid[rank], NULL, monte_carlo, (void *)rank);
+    for (rank = 0; rank < num_of_threads; rank++)
+        pthread_join(tid[rank], NULL);
+    p = n_crossed / (double)iterations;
     pi = 2.0 / (p * l);
     clock_t end = clock();
     float seconds = (float)(end - start) / CLOCKS_PER_SEC;
@@ -41,59 +37,19 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-void monte_carlo(structure_data *data, pthread_t *threads, double l, long long iterations)
+void *monte_carlo(void *id_arg)
 {
-
-    pthread_mutex_init(&mutex, NULL);
-    for (int i = 0; i < MAX_THREADS; i++)
-    {
-        data = (structure_data *)malloc(sizeof(structure_data));
-        data->iterations = iterations / MAX_THREADS;
-        data->total_iterations = iterations;
-        data->l = l;
-        data->thread = i;
-        pthread_create(&(threads[i]), NULL, needles, (void *)data);
-    }
-    for (int i = 0; i < MAX_THREADS; i++)
-        pthread_join(threads[i], NULL);
-    pthread_mutex_destroy(&mutex);
-}
-
-void *needles(void *estructura)
-{
-    structure_data *data = (structure_data *)estructura;
+    long id = (long)id_arg;
+    int size_per_thr = iterations / num_of_threads;
+    int start_index = id * size_per_thr;
+    int final_index = (id + 1) * size_per_thr;
     double x, theta, n;
-    long long iterations = data->iterations;
-    double l = data->l;
-    for (long long i = 0; i < iterations; i++)
+    for (long long i = start_index; i < final_index; i++)
     {
         x = (double)rand() / (double)(RAND_MAX / l);
         theta = (double)rand() / (double)(RAND_MAX / M_PI);
         n = sin(theta) / 2.0;
         if (x + n >= l || x - n <= 0)
-        {
-            pthread_mutex_lock(&mutex);
-            add = add + 1;
-            pthread_mutex_unlock(&mutex);
-        }
+            n_crossed++;
     }
-    if (data->thread == 0)
-    {
-        double resto = data->total_iterations % MAX_THREADS;
-
-        for (int i = 0; i < resto; i++)
-        {
-            x = (double)rand() / (double)(RAND_MAX / l);
-            theta = (double)rand() / (double)(RAND_MAX / M_PI);
-            n = sin(theta) / 2.0;
-            if (x + n >= l || x - n <= 0)
-            {
-                pthread_mutex_lock(&mutex);
-                add = add++;
-                pthread_mutex_unlock(&mutex);
-            }
-        }
-    }
-
-    return NULL;
 }
